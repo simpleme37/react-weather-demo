b036bf842799935a2196840e4e0cfaea1770e67c
simpleme37 <simpleme307@gmail.com>
2025-09-17 16:17:25 +0800

    fix: remove console logs, update font-family declaration

--- app/page.js
+++ app/page.js
@@ -8,9 +8,10 @@ export default function Home() {
   const [forecast, setForecast] = useState(null);
   const [error, setError] = useState("");
   const [jumping, setJumping] = useState(null);
-
+  const [isLoading, setIsLoading] = useState(false);
   const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 
+  // 經緯度資料
   const cityCoordinates = {
     Taipei: { lat: 25.0478, lon: 121.5319 },
     Taichung: { lat: 24.1477, lon: 120.6736 },
@@ -30,6 +31,7 @@ export default function Home() {
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
 
+  // 城市變數設定
   const [tempCity, setTempCity] = useState("Taipei");
   const [selectedCity, setSelectedCity] = useState("Taipei");
   const [coordinates, setCoordinates] = useState(cityCoordinates["Taipei"]);
@@ -50,39 +52,42 @@ export default function Home() {
     setTimeout(() => setJumping(null), 500); // 動畫結束後移除類
   };
 
-  useEffect(() => {
-    const fetchAllData = async () => {
-      try {
-        // 當下資料
-        const currentResponse = await fetch(
-          `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.lat}&lon=${coordinates.lon}&units=metric&appid=${API_KEY}`
-        );
-        if (!currentResponse.ok) {
-          throw new Error("Failed to fetch currentData");
-        }
-        const currentData = await currentResponse.json();
-        console.log("Current data:", currentData); // 檢查
-        const processedCurrentData = processCurrentData(currentData);
-        console.log("Processed Current data:", processedCurrentData); // 檢查
-        setCurrentWeather(processedCurrentData);
-
-        // 預報資料
-        const forecastResponse = await fetch(
-          `https://api.openweathermap.org/data/2.5/forecast?lat=${coordinates.lat}&lon=${coordinates.lon}&units=metric&appid=${API_KEY}`
-        );
-        if (!forecastResponse.ok) {
-          throw new Error("Failed to fetch forecastData");
-        }
-        const forecastData = await forecastResponse.json();
-        console.log("Raw forecast data:", forecastData); // 檢查
-        const processedData = processForecastData(forecastData);
-        console.log("Processed forecast data:", processedData); // 檢查
-        setForecast(processedData);
-      } catch (err) {
-        setError(err.message);
+  const fetchAllData = async () => {
+    setIsLoading(true);
+    try {
+      // 當下資料
+      const currentResponse = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.lat}&lon=${coordinates.lon}&units=metric&appid=${API_KEY}`
+      );
+      if (!currentResponse.ok) {
+        throw new Error("Failed to fetch currentData");
       }
-    };
+      const currentData = await currentResponse.json();
+      // console.log("Current data:", currentData); // 檢查
+      const processedCurrentData = processCurrentData(currentData);
+      // console.log("Processed Current data:", processedCurrentData); // 檢查
+      setCurrentWeather(processedCurrentData);
 
+      // 預報資料
+      const forecastResponse = await fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?lat=${coordinates.lat}&lon=${coordinates.lon}&units=metric&appid=${API_KEY}`
+      );
+      if (!forecastResponse.ok) {
+        throw new Error("Failed to fetch forecastData");
+      }
+      const forecastData = await forecastResponse.json();
+      // console.log("Raw forecast data:", forecastData); // 檢查
+      const processedData = processForecastData(forecastData);
+      // console.log("Processed forecast data:", processedData); // 檢查
+      setForecast(processedData);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setTimeout(() => setIsLoading(false), 1000);
+    }
+  };
+
+  useEffect(() => {
     fetchAllData();
   }, [coordinates]);
 
@@ -162,18 +167,6 @@ export default function Home() {
   }
 
   // 整理 currentData 的函數
-  // function processCurrentData(data) {
-  //   return {
-  //     ...data,
-  //     main: {
-  //       ...data.main,
-  //       temp: Math.round(data.main.temp),
-  //       temp_max: Math.round(data.main.temp_max),
-  //       temp_min: Math.round(data.main.temp_min),
-  //     },
-  //   };
-  // }
-
   function processCurrentData(data) {
     return {
       city: data.name,
@@ -187,151 +180,174 @@ export default function Home() {
 
   return (
     <>
-      <div className={styles.container}>
-        {/* 區塊一 */}
-        {currentWeather && (
-          <div className={styles.main_container}>
-            <span className={styles.sticker}>TEMP NOW</span>
-            <p className={styles.main_temp}>{currentWeather.temp}°</p>
-            <p className={styles.main_city_name} onClick={openModal}>
-              {selectedCity}
-            </p>
-            <div>
-              <table className={styles.info_table}>
-                <thead></thead>
-                <tbody>
-                  <tr>
-                    <td className={`${styles.table_td} ${styles.table_field}`}>
-                      Low Temp
-                    </td>
-                    <td
-                      className={`${styles.table_td} ${styles.table_content}`}
-                    >
-                      {currentWeather.temp_min}°
-                    </td>
-                  </tr>
-                  <tr>
-                    <td className={`${styles.table_td} ${styles.table_field}`}>
-                      High Temp
-                    </td>
-                    <td
-                      className={`${styles.table_td} ${styles.table_content}`}
-                    >
-                      {currentWeather.temp_max}°
-                    </td>
-                  </tr>
-                  <tr>
-                    <td className={`${styles.table_td} ${styles.table_field}`}>
-                      Humidity
-                    </td>
-                    <td
-                      className={`${styles.table_td} ${styles.table_content}`}
-                    >
-                      {currentWeather.humidity}%
-                    </td>
-                  </tr>
-                  <tr>
-                    <td className={`${styles.table_td} ${styles.table_field}`}>
-                      Condition
-                    </td>
-                    <td
-                      className={`${styles.table_td} ${styles.table_content}`}
-                    >
-                      {currentWeather.weather}
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
+      <div className={styles.viewport_center}>
+        {/* 錯誤提醒 */}
+        {error && (
+          <div className={styles.container}>
+            <p className="styles.error">{error}</p>
           </div>
         )}
+        {/* 主要內容 */}
+        <div className={styles.container}>
+          {/* Loader */}
+          {isLoading ? (
+            <div className={styles.lds_ellipsis}>
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          ) : (
+            <div>
+              {/* 區塊一 */}
+              {currentWeather && (
+                <div className={styles.main_container}>
+                  <span className={styles.sticker}>TEMP NOW</span>
+                  <p className={styles.main_temp}>{currentWeather.temp}°</p>
+                  <p className={styles.main_city_name} onClick={openModal}>
+                    {selectedCity}
+                  </p>
+                  <div>
+                    <table className={styles.info_table}>
+                      <thead></thead>
+                      <tbody>
+                        <tr>
+                          <td
+                            className={`${styles.table_td} ${styles.table_field}`}
+                          >
+                            Low Temp
+                          </td>
+                          <td
+                            className={`${styles.table_td} ${styles.table_content}`}
+                          >
+                            {currentWeather.temp_min}°
+                          </td>
+                        </tr>
+                        <tr>
+                          <td
+                            className={`${styles.table_td} ${styles.table_field}`}
+                          >
+                            High Temp
+                          </td>
+                          <td
+                            className={`${styles.table_td} ${styles.table_content}`}
+                          >
+                            {currentWeather.temp_max}°
+                          </td>
+                        </tr>
+                        <tr>
+                          <td
+                            className={`${styles.table_td} ${styles.table_field}`}
+                          >
+                            Humidity
+                          </td>
+                          <td
+                            className={`${styles.table_td} ${styles.table_content}`}
+                          >
+                            {currentWeather.humidity}%
+                          </td>
+                        </tr>
+                        <tr>
+                          <td
+                            className={`${styles.table_td} ${styles.table_field}`}
+                          >
+                            Condition
+                          </td>
+                          <td
+                            className={`${styles.table_td} ${styles.table_content}`}
+                          >
+                            {currentWeather.weather}
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              )}
 
-        {/* 區塊二 */}
-        <div className={styles.subtitle_container}>
-          <span className={styles.sub_divider}></span>
-          <span className={styles.subtitle}>
-            Next Few Days Weather Forecast
-          </span>
-          <span className={styles.sub_divider}></span>
+              {/* 區塊二 */}
+              <div className={styles.subtitle_container}>
+                <span className={styles.sub_divider}></span>
+                <span className={styles.subtitle}>
+                  Next Few Days Weather Forecast
+                </span>
+                <span className={styles.sub_divider}></span>
+              </div>
+
+              {/* 區塊三 */}
+              {forecast && (
+                <div className={styles.card_container}>
+                  {forecast.map((day, i) => (
+                    <div className={styles.card} key={i}>
+                      <p className={styles.card_date}>{forecast[i].date}</p>
+                      <div className={styles.card_main_info}>
+                        <div className={styles.card_bg}>
+                          <Image
+                            src={`/icons/${forecast[i].icon}.png`}
+                            alt="Weather icon"
+                            width={80}
+                            height={80}
+                            loading="lazy"
+                            className={`${styles.card_icon} ${
+                              jumping === i ? styles.jump : ""
+                            }`}
+                            onClick={() => handleIconClick(i)}
+                          ></Image>
+                        </div>
+                        <p className={styles.card_paragraph}>
+                          {forecast[i].minTemp}°
+                          <span className={styles.text_divider}></span>
+                          {forecast[i].maxTemp}°
+                        </p>
+                        <p className={styles.card_sm_paragraph}>
+                          {forecast[i].weather}
+                        </p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+          )}
         </div>
 
-        {/* 區塊三 */}
-        {forecast && (
-          <div className={styles.card_container}>
-            {forecast.map((day, i) => (
-              <div className={styles.card} key={i}>
-                <p className={styles.card_date}>{forecast[i].date}</p>
-                <div className={styles.card_main_info}>
-                  <div className={styles.card_bg}>
-                    <Image
-                      src={`/icons/${forecast[i].icon}.png`}
-                      alt="Weather icon"
-                      width={80}
-                      height={80}
-                      className={`${styles.card_icon} ${
-                        jumping === i ? styles.jump : ""
-                      }`}
-                      onClick={() => handleIconClick(i)}
-                    ></Image>
-                  </div>
-                  <p className={styles.card_paragraph}>
-                    {forecast[i].minTemp}°
-                    <span className={styles.text_divider}></span>
-                    {forecast[i].maxTemp}°
-                  </p>
-                  <p className={styles.card_sm_paragraph}>
-                    {forecast[i].weather}
-                  </p>
-                </div>
+        {/* Modal */}
+        {isModalOpen && (
+          <div className={styles.modal_overlay}>
+            <div className={styles.modal}>
+              <p className={styles.modal_title}>Select City</p>
+              <div className={styles.custom_select}>
+                <select
+                  name="cities"
+                  value={tempCity}
+                  onChange={handleCityChange}
+                  className={styles.styled_select}
+                >
+                  {options.map((city) => (
+                    <option key={city} value={city}>
+                      {city}
+                    </option>
+                  ))}
+                </select>
               </div>
-            ))}
+              <div className={styles.button_wrapper}>
+                <button
+                  onClick={closeModal}
+                  className={styles.modal_button_border}
+                >
+                  Close
+                </button>
+                <button
+                  className={styles.modal_button_bg}
+                  onClick={confirmCityChange}
+                >
+                  Confirm
+                </button>
+              </div>
+            </div>
           </div>
         )}
       </div>
-
-      {/* 錯誤提醒 */}
-      {error && (
-        <div className={styles.container}>
-          <p className="styles.error">{error}</p>
-        </div>
-      )}
-
-      {/* Modal */}
-      {isModalOpen && (
-        <div className={styles.modal_overlay}>
-          <div className={styles.modal}>
-            <p className={styles.modal_title}>Select City</p>
-            <div className={styles.custom_select}>
-              <select
-                name="cities"
-                value={tempCity}
-                onChange={handleCityChange}
-                className={styles.styled_select}
-              >
-                {options.map((city) => (
-                  <option key={city} value={city}>
-                    {city}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div className={styles.button_wrapper}>
-              <button
-                onClick={closeModal}
-                className={styles.modal_button_border}
-              >
-                Close
-              </button>
-              <button
-                className={styles.modal_button_bg}
-                onClick={confirmCityChange}
-              >
-                Confirm
-              </button>
-            </div>
-          </div>
-        </div>
-      )}
     </>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY,
   },
+  compiler: {
+    removeConsole: {
+      exclude: ["error"], // 保留 console.error
+    },
+  },
 };
 
 export default nextConfig;
